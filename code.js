5585f3dd5439d091b59f2e0e9cde56a3b1784244
Emilio Benavides <emilio.benavides68@gmail.com>
2021-12-16 15:23:22 -0600

    Completed 1-6 intro-to-test exercises

--- code.js
+++ code.js
@@ -3,7 +3,15 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
+function sayHello(name) {
+    if (name === "Alex") {
+        return "Hello, Alex!";
+    } else if (name === "Jane") {
+        return "Hello, Jane!";
+    } else {
+        return "Hello, Pat!";
 
-}
+        }
+    }
+
 
